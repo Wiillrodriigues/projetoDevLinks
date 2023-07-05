ecdc6851c21dae07427fc4d16f334bce86731015
Wiillrodriigues <86089098+Wiillrodriigues@users.noreply.github.com>
2023-07-05 18:59:53 -0300

    a

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 function toggleMode() {
-    
-        const html = document.documentElement
-    
-        // if (html.classList.contains('light')){
-        //     html.classList.remove('light')
-        // } else {
-        //     html.classList.add('light')
-        // }
-    
-     html.classList.toggle('light')
+  const html = document.documentElement;
+
+  // if (html.classList.contains('light')){
+  //     html.classList.remove('light')
+  // } else {
+  //     html.classList.add('light')
+  // }
+
+  html.classList.toggle("light");
 }
 
-
 function toggleEbookList() {
-        var ebookList = document.getElementById("ebook-list");
-        if (ebookList.style.display === "none") {
-          ebookList.style.display = "block";
-        } else {
-          ebookList.style.display = "none";
-        }
-      }
-      
+  var ebookList = document.getElementById("ebook-list");
+  if (ebookList.style.display === "none") {
+    ebookList.style.display = "block";
+  } else {
+    ebookList.style.display = "none";
+  }
+}
+
+function downloadEbook(url) {
+  window.open(url);
+}
